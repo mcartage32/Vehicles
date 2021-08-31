9ed8de576813e0cc378e23b9521d304449897941
mcartage32 <mcartage33@gmail.com>
2021-08-30 23:04:11 -0500

    Create DB and Controller

---
+++ VehiclesAPI/Data/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VehiclesAPI.Data.Entities;
+
+namespace VehiclesAPI.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options): base(options)
+        { }
+
+
+        public DbSet<VehicleType> VehicleTypes { get; set; }// VehicleTypes es la coleccion de los tipos de vehiculo, debe ser plural
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<VehicleType>().HasIndex(x => x.Description).IsUnique(); //se define que el campo descripcion es unico
+        }
+    }
+}
---
+++ VehiclesAPI/Data/Entities/VehicleType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VehiclesAPI.Data.Entities
+{
+    public class VehicleType
+    {
+        public int Id { get; set; }//PRIMARY KEY
+
+        [Display(Name = "Tipo de vehículo")]//ASI LE APARECERA AL USUARIO
+        [MaxLength(50, ErrorMessage ="El campo {0} no puede tener mas de {1} caracteres.")]//LIMITAMOS LA CANTIDAD DE CARACTERES
+        [Required(ErrorMessage ="El campo {0} es obligatorio.")]//MENSAJE DE ERROR
+        public string Description { get; set; }
+
+
+
+    }
+}
